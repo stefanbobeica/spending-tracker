ee27362762575b80082e5ec47d690e42d7dfb62d
stefanbobeica <stefanbobeica25@gmail.com>
2025-10-30 20:28:50 +0200

    added improvements

--- Spending Tracker/obj/Debug/net9.0-android/Spending Tracker.AssemblyInfo.cs
+++ Spending Tracker/obj/Debug/net9.0-android/Spending Tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spending Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4b756c8ccd58cb6182886838e23c56fc7376d057")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spending Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spending Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
